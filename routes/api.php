f296b4fb19f9ca54c786c47ffde695527b9eb701
CodeAWProject <activityware@gmail.com>
2023-12-27 14:48:20 +0100

    Defining routes for the Card and Task controllers

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CardController;
+use App\Http\Controllers\Api\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::apiResource('cards', CardController::class);
+Route::apiResource('cards.tasks', TaskController::class)
+    ->scoped(['task' => 'card']);
